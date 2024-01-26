844d1d232f77b1857e34dd8259d3198ebda6b29f
Joha Wilson <johawilsonatu@icloud.com>
2024-01-26 15:41:51 +0000

    create GameBoard component and add logic to display board layout

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,25 +1,30 @@
 import { useState } from "react";
 
-export default function Player({name, symbol}) {
+export default function Player({initialName, symbol}) {
 
-    const [ isEditing, setIsEditing] = useState(false)
+    const [playerName, setPlayerName] = useState(initialName);
+
+    const [ isEditing, setIsEditing] = useState(false);
 
     function handleEditClick() {
 
-        setIsEditing(true);
+        setIsEditing((editing) => !editing);
+    }
+
+    function handleChange(event) {
+        setPlayerName(event.target.value);
     }
 
     return (
         <li>
             <span className="player">
                 { isEditing ? 
-                <input type="text" required /> : 
-                <span className="player-name"> {name}</span>
+                <input type="text" value={playerName} onChange={handleChange} required /> : 
+                <span className="player-name"> {playerName}</span>
                 }
                 <span className="player-symbol"> {symbol} </span>
-
             </span>
-            <button onClick={handleEditClick}>Edit</button>
+            <button onClick={handleEditClick}>{ isEditing ? 'Save' : 'Edit' }</button>
           </li>
 
     );
